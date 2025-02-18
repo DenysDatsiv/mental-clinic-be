4b740ecf1c59a1d1d738d5bd9e8e41b1284d79c5
DenysDatsiv <dendatsiv@gmail.com>
2025-02-18 15:25:51 +0200

    add secure conection to db

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require('cors'); // Import cors
 const connectDB = require('./config/db');
 const testRoutes = require('./routes/testRoutes');
 
+// Swagger dependencies
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 dotenv.config();
 
 const app = express();
@@ -21,6 +25,29 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 };
 app.use(cors(corsOptions));
+
+// Swagger setup
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+            description: 'A simple Express API documentation using Swagger',
+        },
+        servers: [
+            {
+                url: `http://localhost:${process.env.PORT || 5000}`,
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Path to the API docs (can be files or directories)
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+// Serve Swagger API documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use('/api/tests', testRoutes);
